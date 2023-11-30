7c873e417efc19e3fc79631154a74bbdfb469b79
Spencer <other.spencersolberg@outlook.com>
2023-11-29 20:01:07 -0500

    Added list of user pages to website, separate signup page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,36 @@
-import SignupForm from "../islands/SignupForm.tsx";
-
-export default function Home() {
+export default async function Home() {
+  const users = await getUsers();
   return (
     <>
       <h1>pubnix/</h1>
-      <h2>Signup</h2>
-      <SignupForm />
+      <h2>Home</h2>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="/signup">Signup</a>
+          </li>
+        </ul>
+      </nav>
+
+      <p>Users:</p>
+      <ul>
+        {users.map(user => (<>
+          <li>
+            <a href={`https://${user}.pubnix`}>{user}/</a>
+          </li>
+        </>))}
+      </ul>
     </>
   );
+}
+
+const getUsers = async (): Promise<string[]> => {
+    // load list of users from /home directory
+    const users: string[] = [];
+    for await (const entry of Deno.readDir("/home")) {
+      users.push(entry.name);
+    }
+
+    return users;
 }
---
+++ routes/signup.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "../islands/SignupForm.tsx";
+
+export default function SignUp() {
+    return (<>
+        <h1>pubnix/</h1>
+        <h2>Signup</h2>
+        <SignupForm />
+    </>)
+}
