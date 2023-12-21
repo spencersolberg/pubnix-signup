9e46a1fa2d879d9a22852c679c8db4897e8513e6
Spencer <other.spencersolberg@outlook.com>
2023-12-20 18:06:46 -0600

    Remove connected domains on account delete

--- routes/api/account/delete.ts
+++ routes/api/account/delete.ts
@@ -1,6 +1,8 @@
 import { deleteUser } from "../../../utils/users.ts";
 import { Handlers } from "$fresh/server.ts";
 import { getVerificationFromRequest } from "../../../utils/jwt.ts";
+import { getDomains, removeDomain } from "../../../utils/kv.ts";
+import { disconnectDomain } from "../../../utils/domain.ts";
 
 export const handler: Handlers = {
     GET() {
@@ -25,6 +27,11 @@ export const handler: Handlers = {
 
         try {
             await deleteUser(name);
+            const connections = await getDomains(name);
+            for (const connection of connections) {
+                await disconnectDomain(connection);
+                await removeDomain(name, connection.domain);
+            }
         } catch (error) {
             return new Response(null, {
                 status: 400,
